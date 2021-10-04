cd63b18dad8c3f865f43bf26560b4181f2c823e5
imran-de <stimran4405@gmail.com>
2021-10-04 12:09:34 +0600

    homepage explore add using fake data

---
+++ src/components/ExploreCard/ExploreCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ExploreCard = (props) => {
+    const { name, courses, thumbnail } = props.course
+    console.log(props)
+    return (
+        <article className='col'>
+            <div className="row shadow rounded h-100">
+                <div className="col-md-4 d-flex align-items-center">
+                    <img src={thumbnail} className="img-fluid h-75" alt="..." />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text"><small className="text-muted">{courses} Courses</small></p>
+                    </div>
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default ExploreCard;
--- src/components/HomeExplore/HomeExplore.js
+++ src/components/HomeExplore/HomeExplore.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ExploreCard from '../ExploreCard/ExploreCard';
 
 const HomeExplore = () => {
+    const [explore, setExplore] = useState([])
+    useEffect(() => {
+        fetch('fakeCourseHome.json')
+            .then(res => res.json())
+            .then(data => setExplore(data))
+    }, [])
     return (
-        <section>
-
+        <section className='container'>
+            <h1 className='display-4 py-5'>Explore Learn Without Limits</h1>
+            <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-5">
+                {
+                    explore.map(course => <ExploreCard key={course.id} course={course}></ExploreCard>)
+                }
+            </div>
         </section>
     );
 };
